2accb03524e7b69a4210f1f5d56068effbf5f40f
Ryan Weise <weiserd@msoe.edu>
2017-02-08 17:55:55 -0600

    Added src folder and simple number text file

---
+++ src/statistics/StatisticsClient.java
@@ -0,0 +1,13 @@
+package statistics;
+
+/**
+ * Created by weiserd on 2/8/2017.
+ */
+public class StatisticsClient {
+    public final String NUMBER_FILE = "numbers.txt";
+
+    public static void main(String[] args) {
+        //StatisticCalculator calculator = new StatisticCalculator(NUMBER_FILE);
+        //Do stuff with it
+    }
+}
